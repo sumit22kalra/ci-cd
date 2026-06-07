25712e4100c587f1c1154b97b3501b68f10f8a05
Sumit Kalra <sumit22kalra@gmail.com>
2026-06-07 12:26:40 +0530

    Fixed Jenkinsfile

--- employee-management-app/src/app/app.spec.ts
+++ employee-management-app/src/app/app.spec.ts
@@ -14,10 +14,15 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, employee-management-app');
-  });
+ it('should render title', async () => {
+  const fixture = TestBed.createComponent(App);
+  fixture.detectChanges();
+
+  await fixture.whenStable();
+
+  const compiled = fixture.nativeElement as HTMLElement;
+
+  expect(compiled.querySelector('h1')?.textContent)
+    .toContain('Employee Management App');
+});
 });
